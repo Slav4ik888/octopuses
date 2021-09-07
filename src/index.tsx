c296625c4595affef40000a350cc2fbb27003948
Vyacheslav <korzan.va@mail.ru>
2021-09-07 21:45:22 +0800

    fix4 webpack.config

--- src/index.tsx
+++ src/index.tsx
@@ -22,4 +22,4 @@ ReactDOM.render(
   , document.getElementById('root'));
  
   
-// git add . && git commit -m "fix2 webpack.config" && git push -u origin master
+// git add . && git commit -m "fix4 webpack.config" && git push -u origin master
